895804569103d961a880dd194c43c089d22bdf86
Jeongsoo Park <toracle@gmail.com>
2024-09-09 00:04:06 +0900

    apply readline to gptman shell (#1)
    ref #1

--- gptman/main.py
+++ gptman/main.py
@@ -1,5 +1,7 @@
 import time
-import json
+import os
+from contextlib import contextmanager
+
 from openai import OpenAI
 from gptman.prompt import read_settings
 
@@ -14,28 +16,49 @@ def update_instruction(client: OpenAI, asst_id: str, **kwargs):
     return client.beta.assistants.update(asst_id, **kwargs)
 
 
+@contextmanager
+def with_history():
+    histfile = os.path.join(os.path.expanduser('~'), '.gptman_history')
+    try:
+        import readline
+        readline.read_history_file(histfile)
+        readline.set_history_length(1000)
+    except (FileNotFoundError, ImportError):
+        pass
+
+    yield
+
+    try:
+        import readline
+        readline.write_history_file(histfile)
+    except (FileNotFoundError, ImportError):
+        pass
+
+
 def run_shell(client: OpenAI, asst_id: str):
-    thread = client.beta.threads.create()
+    with with_history():
+        thread = client.beta.threads.create()
 
-    while True:
-        content = input(f'[{asst_id}] User > ')
-        if content in ['quit', 'exit']:
-            return
+        while True:
+            content = input(f'[{asst_id}] User> ')
+            if content in ['quit', 'exit']:
+                return
 
-        if not content:
-            continue
+            if not content:
+                continue
 
-        client.beta.threads.messages.create(
-            thread_id=thread.id,
-            role='user',
-            content=content,
-        )
+            client.beta.threads.messages.create(
+                thread_id=thread.id,
+                role='user',
+                content=content,
+            )
 
-        generated_message = run_assistant(client, asst_id, thread)
-        print(f'[{asst_id}] GPT >', generated_message)
+            generated_message = run_assistant(client, asst_id, thread)
+            print(f'[{asst_id}] GPT>', generated_message)
 
 
 def run_assistant(client, asst_id, thread):
+    print('.', end='', flush=True)
     run_obj = client.beta.threads.runs.create_and_poll(
         thread_id=thread.id,
         assistant_id=asst_id,
@@ -43,20 +66,16 @@ def run_assistant(client, asst_id, thread):
 
     while True:
         if run_obj.status == 'completed':
+            print('.', flush=True)
             generated_content = get_generated_content(client, thread)
             return generated_content
 
         time.sleep(0.5)
+        print('.', end='', flush=True)
 
 
 def get_generated_content(client, thread):
     messages = client.beta.threads.messages.list(thread_id=thread.id)
     message = messages.data[0]
     value = message.content[0].text.value
-
-    try:
-        json_value = json.loads(value)
-        generated_content = json_value['values'] or json_value['error']
-        return generated_content
-    except (json.JSONDecodeError, KeyError):
-        return value
+    return value
